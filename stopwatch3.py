b0a19b05ad527af5a1d666d6dc47b0f593d1d2e1
dgpb <37355736+dgpb@users.noreply.github.com>
2022-12-31 12:40:14 -0500

    Update stopwatch3.py

--- stopwatch3.py
+++ stopwatch3.py
@@ -4,86 +4,80 @@ import tkinter.font as TkFont
 from datetime import datetime
 import requests
 
-AIRTABLE_BASE_ID='xxxxxxxx'
-AIRTABLE_API_KEY='xxxxxxxx'
+AIRTABLE_BASE_ID='xxxxxx'
+AIRTABLE_API_KEY='xxxxx'
 AIRTABLE_NAME='projects'
 
 
-def run():
-    current_time = datetime.now()
-    diff = current_time - start_time 
-    txt_var.set('%d.%02d' % (diff.seconds,diff.microseconds//10000))
+running = False
+
+seconds, milliseconds = 1, 0
 
 
-
-    if running:
-        root.after(20,run)
-
-
+# start function
 def start():
     global running
-    global start_time
-    global start_pause
-
-
     if not running:
+        update()
         running = True
-        if start_pause is None:
-            start_time = datetime.now()
-            print("start", start_time)
-        else:
-            start_time = start_pause
-            print("new start", start_time)
 
 
-        root.after(10,run)
-
-
-    #print("start_time", start_time)
-
+# pause function
 def stop():
     global running
-    global start_pause
-
-    running = False
-    start_pause = datetime.now()
-
-    print("start_pause", start_pause)
+    if running:
+        # cancel updating of time using after_cancel()
+        stopwatch_label.after_cancel(update_time)
+        running = False
 
 
 
-#def stop():
-    #global running
-    #running = False
-
+# reset function
 def reset():
-    global start_time
-    start_time = datetime.now()
+    global running
+    if running:
+        # cancel updating of time using after_cancel()
+        stopwatch_label.after_cancel(update_time)
+        running = False
+    # set variables back to zero
+    global seconds, milliseconds
+    seconds, milliseconds = 1, 0
+    # set label back to zero
+    stopwatch_label.config(text='0.00')
 
-    if not running:
-        txt_var.set('0.00')
+# update stopwatch function
+def update():
+    # update seconds with (addition) compound assignment operator
+    global seconds, milliseconds
+    milliseconds += 1
+    #if seconds == 60:
+    #    minutes += 1
+    #    seconds = 0
+    if milliseconds == 99:
+        seconds += 1
+        milliseconds = 0
+    # format time to include leading zeros
+    #hours_string = f'{hours}' if hours > 9 else f'0{hours}'
+    #minutes_string = f'{minutes}' if minutes > 9 else f'0{minutes}'
+    seconds_string = f'{seconds}'
+    milliseconds_string = f'{milliseconds}' if milliseconds > 99 else f'{milliseconds}'
+    # update timer label after 1000 ms (1 second)
+    stopwatch_label.config(text=seconds_string + '.' + milliseconds_string)
+    # after each second (1000 milliseconds), call update function
+    # use update_time variable to cancel or pause the time using after_cancel
+    global update_time
+    global last_update
+    update_time = stopwatch_label.after(10, update)
 
-
-running = False
-start_time = None
-start_pause = None
-
-
-root = tk.Tk()
-root.geometry("390x500")
-root.title("Airtable Project Tracker")
-
-str = tk.StringVar()
+    last_update = seconds_string + '.' + milliseconds_string
 
 def update_arch():
 
     recId = str.get()
     print(recId)
 
-    current_time = datetime.now()
-    tm = current_time - start_time
-    tm = txt_var.get()
-    print(tm)
+    print(last_update)
+
 
     endpoint=f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_NAME}'
 
@@ -98,7 +92,7 @@ def update_arch():
         {
           "id": recId,
           "fields": {
-            "Archiving Time": float(tm)
+            "Archiving Time": float(last_update)
           }
         },
       ]
@@ -109,37 +103,69 @@ def update_arch():
 
     root.destroy()
 
+def update_send():
+
+    recId = str.get()
+    print(recId)
+
+    print(last_update)
+
+
+    endpoint=f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_NAME}'
+
+
+    headers = {
+        "Authorization" : f"Bearer {AIRTABLE_API_KEY}",
+        "Content-Type" : "application/json"
+    }
+
+    data = {
+      "records": [
+        {
+          "id": recId,
+          "fields": {
+            "Sending Time": float(last_update)
+          }
+        },
+      ]
+    }
+
+    r = requests.patch(endpoint, json=data, headers=headers)
+    print(r.json())
+
+    root.destroy()
+
+# create main window
+root = tk.Tk()
+root.geometry('350x420')
+root.title('Project Tracker')
+
+str = tk.StringVar()
+
+# label to display time
+stopwatch_label = tk.Label(text='0.00', font=('System', 80))
+stopwatch_label.pack()
+
+# start, pause, reset, quit buttons
+start_button = tk.Button(text='start', width=30, font=('Arial', 10), command=start)
+start_button.pack(pady = 1)
+stop_button = tk.Button(text='stop', width=30, font=('Arial', 10), command=stop)
+stop_button.pack(pady = 1)
+reset_button = tk.Button(text='reset', width=30, font=('Arial', 10), command=reset)
+reset_button.pack(pady = 1)
 
 # Id Record Entry box
 tk.Label(root, text="Enter Record ID : ", font='Helvetica 10 bold').pack(pady = 5)
 
-# Get Id project form Record in Airtable
-recid_entry = tk.Entry(root, textvariable = str, width=50)
+recid_entry = tk.Entry(textvariable = str, width=30)
 recid_entry.pack(pady = 10)
 
+archived_button = tk.Button(text='Archived', width=30, font=('Arial', 10), command=update_arch, bg="#6eb9f3")
+archived_button.pack(pady = 10)
+sent_button = tk.Button(text='Sent', width=30, font=('Arial', 10), command=update_send, bg="#ffa981")
+sent_button.pack()
 
-txt_var = tk.StringVar()
-txt_var.set('0.00')
-
-
-fontStyle = TkFont.Font(family="Terminal", size = 10)
-tk.Label(root, textvariable=id,font = fontStyle)
-tk.Label(root, textvariable=txt_var,font = fontStyle).pack(pady = 50)
-
-
-
-
-# Track time buttons
-tk.Button(root, text="Start",command = start, width=50, bg="#b1b1b1").pack(pady = 5)
-tk.Button(root, text="Stop",command = stop, width=50, bg="#b1b1b1").pack(pady = 5)
-tk.Button(root, text="Reset",command = reset, width=50, bg="#b1b1b1").pack(pady = 5)
-
-# Pull record data Button
-id_button = tk.Button(root, text="Archived", width=50, bg="#6eb9f3", command = update_arch)
-id_button.pack(pady = 5)
-
-id_button = tk.Button(root, text="Sent", width=50, bg="#ffa981")
-id_button.pack(pady = 5)
-
+# run app
 root.mainloop()
+
 
